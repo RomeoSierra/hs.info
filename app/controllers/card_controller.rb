d7fc8d5e815dfae10611679102abe5c28a2d7594
RomeoSierra <rayswr@gmail.com>
2016-03-05 22:56:52 -0500

    Thanks Andrew
    now with helpful comments

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -8,27 +8,16 @@ class CardController < ApplicationController
 	
 
 	def index
-  		if params[:search]
-    		@cards = Card.search(params[:search])
-  		else
-    		@cards = Card.all
-  		end
+		@cards = Card.complex_query(params)
+
+
+		# when query_hash.present?
+
+  # 		when params[:search].present?
+  #   		@cards = Card.search(params[:search])
+  # 		else
+  #   		@cards = Card.all
+  # 		end
 	end
 	
-
-	def search
-  		@cards = Card.search(params[:search])
-	end
-
-
-	def complex_search(q_name, q_set, q_rarity, q_align, q_type, q_subtype, q_cost_val, q_cost_mod, q_power_val, q_power_mod, q_toughness_val, q_toughness_mod, q_card_text, q_wild_legal, q_kraken_legal)
-		@q_results = Card.all
-
-		if q_name.present?
-
-		else
-			
-		end
-
-	end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -11,80 +11,15 @@ class Card < ActiveRecord::Base
   		where("card_name like ?", "%#{q}%") 
 	end
 
+	def self.complex_query(query_hash) 
 
-	# def results
-	# 	@results ||= find_cards
-	# end
+		q_results = Card.all
 
+	# 	results = results.where{ "name like Q", query_hash[:q_name] } if queryHash[:q_name].present?
+	# 	results = resulsaf
 
-	# private
+		return q_results
+	end
 
-	# def find_cards
-	# 	Card.find(:all, :conditions => conditions)
-	# end
-
-	# def card_name_conditions
-	# 	["card.card_name LIKE ?", "%#{keywords}%"] unless keywords.blank?
-	# end
-
-	# def align_conditions
-	# 	["card.align LIKE ?", "%#{q_align}%"] unless q_align.blank? #allow selecting multiple, return cards that match any
-	# end
-
-	# def rarity_conditions
-	# 	["card.align LIKE ?", "%#{q_rarity}%"] unless q_rarity.blank? #allow selecting multiple, return cards that match any
-	# end
-
-	# def set_conditions
-	# 	["card.set LIKE ?", "%#{q_set}%"] unless q_set.blank? #allow selecting multiple, return cards that match any
-	# end
-
-	# def type_conditions
-	# 	["card.type LIKE ?", "%#{q_type}%"] unless q_type.blank? #allow selecting multiple, return cards that match any
-	# end
-
-	# def subtype_conditions
-	# 	["card.subtype LIKE ?", "%#{q_subtype}%"] unless q_subtype.blank? #allow selecting multiple, return cards that match any
-	# end
-
-	# def cost_conditions
-	# 	["card.cost LIKE ?", q_cost] unless q_cost.blank? #need to figure out how to handle equivilancy operators
-	# end
-
-	# def power_conditions
-	# 	["card.power LIKE ?", q_power] unless q_power.blank? #need to figure out how to handle equivilancy operators, also how does this handle nil?
-	# end
-
-	# def toughness_conditions
-	# 	["card.toughness LIKE ?", q_toughness] unless q_toughness.blank? #need to figure out how to handle equivilancy operators, also how does this handle nil?
-	# end
-
-	# def rules_text_conditions
-	# 	["card.card_text LIKE ?", "%#{q_rules_text}%"] unless q_rules_text.blank?
-	# end
-
-	# def collectible_conditions
-	# 	["card.independent LIKE ?", true] unless exclude_depentent.blank?
-	# end
-
-	# # def flavor_text_conditions
-	# # 	["card.flavor_text LIKE ?", "%#{q_flavor_text}%"] unless q_flavor_text.blank?
-	# # end
-
-	# def conditions
- #  		[conditions_clauses.join(' AND '), *conditions_options]
-	# end
-
-	# def conditions_clauses
-	# 	conditions_parts.map { |condition| condition.first }
-	# end
-
-	# def conditions_options
-	# 	conditions_parts.map { |condition| condition[1..-1] }.flatten
-	# end
-
-	# def conditions_parts
-	# 	private_methods(false).grep(/_conditions$/).map { |m| send(m) }.compact
-	# end
-
+	
 end
